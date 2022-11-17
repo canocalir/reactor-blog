13f01b588fc5b9c1a1c6b8d5bc691789d9e1c288
Can Berk Ocalir <can.ocalir@gmail.com>
2022-11-17 04:09:54 +0300

    added env keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9hVbbrCNm1sbRavqReq_nvKm6fI_fm10",
-  authDomain: "blog-milestone-8966d.firebaseapp.com",
-  projectId: "blog-milestone-8966d",
-  storageBucket: "blog-milestone-8966d.appspot.com",
-  messagingSenderId: "922065969911",
-  appId: "1:922065969911:web:4b15493caac31fab6829f2",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
