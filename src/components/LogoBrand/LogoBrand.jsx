480af6971972b8a2c6d1bcc7048ba721377870fd
Can Berk Ocalir <can.ocalir@gmail.com>
2022-11-21 00:36:29 +0300

    changed logo

--- src/components/LogoBrand/LogoBrand.jsx
+++ src/components/LogoBrand/LogoBrand.jsx
@@ -1,15 +1,16 @@
 import { Navbar } from "flowbite-react"
+import logo from '../../assets/mainlogo.png'
 
 const LogoBrand = () => {
   return (
     <Navbar.Brand href="/">
     <img
-      src="https://flowbite.com/docs/images/logo.svg"
-      className="mr-3 h-6 sm:h-9"
-      alt="Flowbite Logo"
+      src={logo}
+      className="mr-3 h-10 sm:h-9"
+      alt="Logo"
     />
     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      Reactor Blog
+      Reactor
     </span>
   </Navbar.Brand>
   )
