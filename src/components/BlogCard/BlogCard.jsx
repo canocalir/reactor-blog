9ed4a88ad016939cf94566bfc489b7bcee50ffd8
Can Berk Ocalir <can.ocalir@gmail.com>
2022-11-21 04:19:55 +0300

    created share button

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,8 +1,8 @@
 import { Button } from "flowbite-react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { CardContainer, CardParagraph } from "./styled";
 import { CommentCount } from "disqus-react";
-import { FcSms } from "react-icons/fc";
+import { FcSms, FcCalendar, FcManager, FcShare } from "react-icons/fc";
 
 const BlogCard = ({ post }) => {
   const {
@@ -12,24 +12,34 @@ const BlogCard = ({ post }) => {
   const disqusConfig = {
     url: "https://gleaming-bunny-a87b8c.netlify.app/",
     identifier: post?.id,
-    title: title
-  }
-  
+    title: title,
+  };
+  const fullURL = `emailto:${author}?subject=sharing link\n&body=I am sharing a link:${disqusConfig?.url}details/${post?.id}}`;
   return (
     <div className="max-w-sm relative">
       <div className="flex items-center absolute right-0">
         <span className="gap-1 flex items-center bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-        <FcSms/>
-        <CommentCount
-          shortname="reactorblog-1"
-          config={disqusConfig}
-        >0 Comments</CommentCount>
+          <FcSms />
+          <CommentCount shortname="reactorblog-1" config={disqusConfig}>
+            0 Comments
+          </CommentCount>
         </span>
-        </div>
+      </div>
+      <div className="flex items-center absolute left-2 top-2 cursor-pointer">
+        <a href={fullURL}>
+          <FcShare size={"1.5rem"} />
+        </a>
+      </div>
       <CardContainer imgAlt="blogImage" imgSrc={image}>
-      <div className="flex w-full justify-between bg-blue-100 text-blue-800 text-xs font-semibold p-1 rounded dark:bg-blue-200 dark:text-blue-800">
-        <p className="text-sm text-gray-500 truncate dark:text-gray-400">{created}</p>
-        <p className="text-sm text-gray-500 truncate dark:text-gray-400">{author}</p>
+        <div className="flex w-full justify-between bg-blue-100 text-blue-800 text-xs font-semibold p-1 rounded dark:bg-blue-200 dark:text-blue-800">
+          <p className="text-sm flex items-center gap-1 text-gray-500 truncate dark:text-gray-400">
+            <FcCalendar />
+            {created}
+          </p>
+          <p className="text-sm flex items-center gap-0 text-gray-500 truncate dark:text-gray-400">
+            <FcManager />
+            {author}
+          </p>
         </div>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
