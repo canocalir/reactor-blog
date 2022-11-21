3a2bf1756855648aec340fce29eb59f6e34a21cb
Can Berk Ocalir <can.ocalir@gmail.com>
2022-11-21 04:20:16 +0300

    created share button

--- src/components/ImageBar/ImageBar.jsx
+++ src/components/ImageBar/ImageBar.jsx
@@ -1,14 +1,19 @@
+import { FcCalendar, FcManager } from "react-icons/fc";
 import { ImageBarContainer, ImageBarDetailsContainer } from "./styled";
 
 const ImageBar = ({ created, author }) => {
   return (
     <ImageBarContainer>
       <ImageBarDetailsContainer>
-        <span>Date Created </span>
-        <p>{created}</p>
+        <span className="flex items-center gap-1"><FcCalendar />Date Created</span>
+        <p>
+          {created}
+        </p>
       </ImageBarDetailsContainer>
       <ImageBarDetailsContainer>
-        <span>Author </span>
+        <span className="flex items-center gap-1">
+          <FcManager /> Author{" "}
+        </span>
         <p>{author}</p>
       </ImageBarDetailsContainer>
     </ImageBarContainer>
